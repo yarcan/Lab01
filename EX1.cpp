031f03bbdaa88b08175fecb03e49d03a8122c342
yyarcan <yusufyarcan@gmail.com>
2022-03-08 13:16:44 +0300

    Exercise 01

--- EX1.cpp
+++ EX1.cpp
@@ -1,3 +1,26 @@
 #define e 2.7182818284590452353602874713526624977572
-
 #include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+double n;
+
+double x,y;
+
+int main() {
+
+	cout << "Enter n: " << endl;
+	cin >> n ;
+
+
+	y = 1 / n;
+	y = y + 1;
+
+	x = pow(y, n);
+
+	cout << "x = " << x << endl;
+
+	return 0;
+
+}
